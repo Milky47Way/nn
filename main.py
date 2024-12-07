690cc7869942e8a916ba3d1b7c3755870ca359f8
Milk <145270243+Milky47Way@users.noreply.github.com>
2024-12-07 13:18:51 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pandas as pd
+from datetime import datetime
+# pip import scikit-learn
+
 from sklearn.model_selection import train_test_split  # Імпортуємо функцію для розділення даних на тренувальну і тестову вибірки
 from sklearn.preprocessing import StandardScaler  # Імпортуємо StandardScaler для нормалізації даних
-from sklearn.neighbors import KNeighborsClassifier  # Імпортуємо K-Nearest Neighbors Classifier
+# Імпортуємо моделі класифікації з sklearn
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier, ExtraTreesClassifier, BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression  # Логістична регресія
+from sklearn.linear_model import RidgeClassifier  # Класифікатор на основі Ridge-регресії
+from sklearn.linear_model import SGDClassifier  # Класифікатор на основі стохастичного градієнтного спуску
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 from sklearn.metrics import confusion_matrix, accuracy_score  # Імпортуємо метрики оцінки моделі
 
 df = pd.read_csv("train.csv")
 print(df.info())
+
 
 # Аналіз кількості покупок залежно від статі
 gender_group = df.groupby("gender")["result"].mean()
@@ -19,19 +33,23 @@ print(relation_group)
 df["bdate"] = pd.to_datetime(df["bdate"], errors="coerce")
 df["bdate_year"] = df["bdate"].dt.year.fillna(df["bdate"].dt.year.median())
 
-
-df['last_seen'] = pd.to_datetime(df['last_seen'], dayfirst=True, errors= 'coerce')
-current_date = datatime.now()
-
-df['days_since_last_seen'] = (current_date - df['last_seen'].dt.days)
-
-
 # Створюємо ознаку віку
 current_year = 2024
 df["age"] = current_year - df["bdate_year"]
 
 # Чи працює користувач
-df["is_employed"] = df["career_end"] > df["career_start"]
+df["is_employed"] = df["career_end"] < df["career_start"]
+
+# створбємо стовпчик із кількістю днів не відвідування
+df['last_seen'] = pd.to_datetime(
+    df['last_seen'], dayfirst=True, errors='coerce')
+
+current_date = datetime.now()
+
+# Обчислюємо кількість днів з моменту останнього візиту
+df['days_since_last_seen'] = (
+        current_date - df['last_seen']).dt.days
+# створбємо стовпчик із кількістю днів не відвідування
 
 df.drop(["id", "last_seen", "langs", "bdate", "career_end", "career_start"], axis=1, inplace=True)
 
@@ -67,7 +85,30 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)  # Нормалізуємо тренувальні дані
 X_test = sc.transform(X_test)  # Нормалізуємо тестові дані на основі параметрів тренувальної вибірки
 
-classifier = KNeighborsClassifier(n_neighbors=5)  # Створюємо модель K-Nearest Neighbors з 5 сусідами
+# classifier = KNeighborsClassifier(n_neighbors=5)  # Створюємо модель K-Nearest Neighbors з 5 сусідами
+
+# classifier = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
+
+# classifier = DecisionTreeClassifier(criterion="gini", max_depth=None, random_state=42)
+
+# classifier = RandomForestClassifier(n_estimators=100, criterion="gini", max_depth=None, random_state=42)
+
+# classifier = LinearDiscriminantAnalysis(solver="svd")
+
+# classifier = LogisticRegression(penalty="l2", solver="lbfgs", max_iter=100, random_state=42)
+
+# classifier = GaussianNB(var_smoothing=1e-9)
+
+# classifier = ExtraTreesClassifier(n_estimators=100, criterion="gini", max_depth=None, random_state=42)
+
+# classifier = BaggingClassifier( n_estimators=10, random_state=42)
+
+# classifier = LogisticRegression(penalty="l2", solver="lbfgs", max_iter=100, random_state=42)
+
+# classifier = RidgeClassifier(alpha=1.0, random_state=42)
+
+classifier = SGDClassifier(loss="log_loss", max_iter=1000, random_state=42)
+
 classifier.fit(X_train, y_train)  # Навчаємо модель на тренувальних даних
 
 y_pred = classifier.predict(X_test)  # Виконуємо прогнозування на тестових даних
@@ -97,7 +138,12 @@ current_year = 2024
 df2["age"] = current_year - df2["bdate_year"]
 
 # Чи працює користувач
-df2["is_employed"] = df2["career_end"] > df2["career_start"]
+df2["is_employed"] = df2["career_end"] < df2["career_start"]
+
+# створбємо стовпчик із кількістю днів не відвідування
+df2['last_seen'] = pd.to_datetime(df2['last_seen'], dayfirst=True, errors='coerce')
+# Обчислюємо кількість днів з моменту останнього візиту
+df2['days_since_last_seen'] = (current_date - df2['last_seen']).dt.days
 
 df2.drop(["id", "last_seen", "langs", "bdate",
           "career_end", "career_start"], axis=1, inplace=True)
@@ -126,3 +172,4 @@ result = pd.DataFrame({'id': ID, 'result': y_pred2})
 
 # зберігаємо в окреми CSV файл наш  результат із лише двома стовпчиками
 result.to_csv("result.csv", index=False)
+
